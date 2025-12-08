a60eacbb794350204340f3f2cadc88ea5e396701
Mathis <mathis.morvant@etu.univ-amu.fr>
2025-12-09 00:58:27 +0100

    Enhance video embedding with inline playback and improve mobile header styles for better usability

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ async function openProjectModal(projectId) {
         content += `
             <div style="width: 100%; max-width: 900px; margin: 2rem auto; position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
                 <iframe 
-                    src="https://www.youtube.com/embed/${project.youtube_id}" 
+                    src="https://www.youtube.com/embed/${project.youtube_id}?playsinline=1&rel=0" 
                     style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                     allowfullscreen>
